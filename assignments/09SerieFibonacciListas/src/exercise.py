c79c92725f5a7ed41ee64f469b0b704c1a69b3c4
A01254123 <A01254123@itesm.mx>
2021-10-05 17:10:36 +0000

    SerieFib_CesarMtz

--- assignments/09SerieFibonacciListas/src/exercise.py
+++ assignments/09SerieFibonacciListas/src/exercise.py
@@ -1,6 +1,29 @@
-def main():
-    
-    pass
-
+def main():     #Fuente: https://www.delftstack.com/es/howto/python/fibonacci-sequence-python/
+    i=-1
+    lista=[]
+    a=1
+    b=1
+    total=0
+    while i<=-1:
+        i=int(input())
+    if i==1:
+        lista.append(0)
+        print(lista)
+    elif i==2:
+        lista.append(0)
+        lista.append(1)
+        print(lista)
+    elif i==0:
+        print(lista)
+    else:
+        lista.append(0)
+        lista.append(1)
+        lista.append(1)
+        for x in range(i-3):
+            total=a+b
+            b=a
+            a=total
+            lista.append(total)
+        print(lista)    
 if __name__=='__main__':
     main()
