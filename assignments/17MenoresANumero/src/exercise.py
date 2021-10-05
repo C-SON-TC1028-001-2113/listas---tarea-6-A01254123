47daec3d5b3db1d453134349db71454eb4058e0f
A01254123 <A01254123@itesm.mx>
2021-10-05 20:45:32 +0000

    MenoresANumero_CesarMtz

--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,9 +1,22 @@
-
+def menores(matriz):
+    menores=[]
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j]<10:
+                menores.append(matriz[i][j])
+    return menores
 
 def main():
-   
-
-
+    elementrosMatriz=[]
+    filas=int(input())
+    columnas=int(input())  
+    for i in range(filas):
+        elementrosMatriz.append([])
+        for j in range(columnas):
+            datosMatriz=int(input())
+            elementrosMatriz[i].append(datosMatriz)
+    menoresDiez=menores(elementrosMatriz)
+    print(menoresDiez)
 
 if __name__=='__main__':
     main()
